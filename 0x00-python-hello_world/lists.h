9d235918431c63cf59b36f2821250300a4de4fae
kirubel-web <kirubelalemu119@gmail.com>
2023-10-30 14:07:32 -0700

    task edit

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -9,7 +9,7 @@
  * * @next: points to the next node
  * *
  * * Description: singly linked list node structure
- * * for Holberton project
+ * * for ALX project
  * *
  */
 typedef struct listint_s
@@ -18,11 +18,9 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
+size_t print_listint(const listint_t *h);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
 #endif /* LISTS_H */
-
-
