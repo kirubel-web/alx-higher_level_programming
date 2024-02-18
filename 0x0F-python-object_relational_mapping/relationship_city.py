bba523cec0c5a7b0ec75ae615ac56956ba8edece
kirubel-web <kirubelalemu119@gmail.com>
2024-02-18 08:54:41 -0800

    pycode

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -17,7 +17,6 @@ class City(Base):
     state_id (int): The state the city belongs to
     """
 
-
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
